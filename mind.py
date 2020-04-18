152beb5f8d3a0a940ae68c384b349e6bc9f7d4cc
deopllj <59074537+deopllj@users.noreply.github.com>
2020-04-18 21:37:45 +0800

    Update mind.py

--- mind.py
+++ mind.py
@@ -3,19 +3,18 @@ import collections
 import os
 import shutil
 import zipfile
-nodes = {}
-i= 0
+#Find the node id in .mindmap file, due to the node in the .mindmap file is unidirectional
 def findid(id):
     for node in ydjson["nodes"]:
         if (node["id"]==id):
             return node
     return 0
+#Recursively build a node tree
 def xmind(ydstruct,xstruct):
     if(ydstruct["id"]!="root"):
         if(not ydstruct["child"]):
             temp = {"id":ydstruct["id"],"title":ydstruct["topic"]}
             xstruct.append(temp)
-#            return "null"
         else:
             temp = {"id":ydstruct["id"],"title":ydstruct["topic"],"children":{"attached":[]}}
             temp1 = temp["children"]["attached"]
@@ -26,13 +25,15 @@ def xmind(ydstruct,xstruct):
         temp1 = xstruct["children"]["attached"]
         for children in ydstruct["child"]:
             xmind(findid(children),temp1)
+#Transfer the .mindmap file into a dict
 with open ('as.mindmap','r') as ydMind:
     ydcontent = ydMind.read()
+#Load the "content.json" in Xmind file, which includes basic config and style in xmind file
 with open ('Templates\XmindJsonTemplate.json','r') as xmindtem:
     xmindjson = xmindtem.read()
 ydjson = json.loads(ydcontent)
+#Xmind is the final "content.json" file's dict pattern
 Xmind = json.loads(xmindjson, object_pairs_hook=collections.OrderedDict)
-i=0
 for node in ydjson["nodes"]:
     node["child"] = []
 for node in ydjson["nodes"]:
@@ -41,6 +42,7 @@ for node in ydjson["nodes"]:
         parent = findid(parentid)
         parent["child"].append(node["id"])
 root = findid("root")
+#configure the Xmind file
 rootid = root["id"]
 rootTopic = Xmind["rootTopic"]
 rootTopic["id"] = root["id"]
@@ -48,6 +50,7 @@ rootTopic["title"] = root["topic"]
 xmind(findid("root"),rootTopic)
 output = []
 output.append(Xmind)
+#Write the json file includes node data into .xmind file
 if(os.path.exists('Output')):
     shutil.rmtree('Output')
 os.mkdir('Output')
